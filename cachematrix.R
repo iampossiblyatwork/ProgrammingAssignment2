edfc300351eb4a82817758936ee228fd2a198a73
iampossiblyatwork <james@costellomail.net>
2021-01-01 14:20:18 -0500

    Re-org data into submission format.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+##makeCacheMatrix stores my incoming data and resets my cache to null, while also defining several functions for getting and setting data.
+##cacheSolve first gets either NULL or inverted data from our stored data and then checks that data to see if it needs to re-run the inverse calculation or not.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function( x = matrix()){
+  cache <-NULL
+  set<- function(y){
+    x<<-y
+    cache <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) cache <<- inverse
+  getinverse <- function() cache
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+  cache <- x$getinverse()
+  if(!is.null(cache)){
+    message("getting cached data")
+    return(cache)
+  }
+  data <- x$get()
+  cache <- solve(data,...)
+  x$setinverse(cache)
+  cache
 }
+
